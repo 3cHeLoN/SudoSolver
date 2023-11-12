7b8443cd03e2d8120afc8cc93250a9d86eb830ec
Folkert Bleichrodt <fbleichrodt@gmail.com>
2023-11-12 22:17:29 +0100

    Fix logging

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,15 @@
+import logging
+
 from sudosolve.reader import SpfLoader
 
 
 def main():
-    loader = SpfLoader("sudokus.spf")
-    sudokus = loader.load()
+    loader = SpfLoader()
+    sudokus = loader.load("sudokus.spf")
     for sudoku in sudokus:
         sudoku.show()
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
